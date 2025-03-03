c626479775115ccf4965672e7bfc3d8cb04f2d1d
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-03-03 04:04:40 -0800

    Initial create

---
+++ routes/commentRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const commentRouter = Router();
+
+/*
+
+commentRouter.post("/create", userGet);
+commentRouter.put("/edit", userGet);
+commentRouter.delete("/delete", userGet);
+
+*/
+
+export default commentRouter;
